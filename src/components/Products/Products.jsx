82bf1894cf8310c009e5aa48657a6649ed446ff0
whittydev <whittymusic@gmail.com>
2021-03-07 22:47:13 -0600

    init commit

---
+++ src/components/Products/Products.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import { Grid } from "@material-ui/core";
+import Product from "./Product/Product";
+import useStyles from './styles';
+
+const products = [
+  { id: 1, name: "Shoes", description: "Running shoes.", price: "$5", image: 'https://www.rei.com/dam/content_team_010818_52427_htc_running_shoes_hero2_lg.jpg'},
+  { id: 2, name: "Macbook", description: "Apple macbook.", price: "$10" , image: 'https://cnet3.cbsistatic.com/img/yjrw7VgWV7a95AvK8Ym0Np4bFXY=/1200x675/2017/06/27/13484418-bfd9-41e2-8f2d-9b4afb072da8/apple-macbook-pro-15-inch-2017-14.jpg'},
+];
+
+const Products = () => {
+  const classes = useStyles();
+  return (
+    <main className={classes.content}>
+      <div className={classes.toolbar}/>
+      <Grid container justify="center" spacing={4}>
+        {products.map((product) => (
+          <Grid item key={product.id} xs={12} sm={6} md={4} lg={3}>
+            <Product product={product} />
+          </Grid>
+        ))}
+      </Grid>
+    </main>
+  );
+};
+
+export default Products;
